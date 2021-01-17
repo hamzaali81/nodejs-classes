236226faf33c1927b1fb2a700cc1c948ec5f36ba
hamzaali <hamza54834@gmail.com>
2021-01-17 15:43:59 +0500

    API: folder structure complete

---
+++ practice-DB/API-Structures/routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const tourController = require('../controllers/tourControllers')
+const aliasTopTours = require('../controllers/tourControllers');
+const router = express.Router();
+// router.param('id',tourController.checkID)
+// router.param('id',tourController.checkID)
+
+router.route('/top-5-cheap')
+.get(tourController.aliasTopTours,tourController.getAllTours);
+
+router.route('/')
+.get(tourController.getAllTours)
+.post(tourController.createTour)
+
+router.route('/:id')
+.get(tourController.getTours)
+.patch(tourController.updateTours)
+.delete(tourController.deleteTours);
+
+module.exports = router;
---
+++ practice-DB/API-Structures/utils/apiFeatures.js
@@ -0,0 +1,115 @@
+// class APIFeatures{
+//     constructor(query, queryString){
+//         this.query = query;
+//         this.queryString = queryString;
+//     }
+//     filter(){
+//         const queryObj = {...this.queryString}
+//         console.log('req.query',req.query);
+//         console.log(queryObj)
+//         const excludedFields = ['page','sort','limit','fields'];
+//         excludedFields.forEach((el)=> delete queryObj[el]);
+       
+//         //1B Advance filtering
+//         let queryStr = JSON.stringify(queryObj); //String Method
+//         queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g ,match => `$${match}`);
+//         // let query =  Tour.find(JSON.parse(queryStr));
+//         // console.log(JSON.parse(queryStr));
+//         this.query.find(JSON.parse(queryStr));
+//         return this; 
+//     }
+
+//     sort(){ 
+//           //?sort=price,ratingsAverage
+//     if(this.queryString.sort){
+//         // log(req.query.sort)
+//         const sortBy = req.query.sort.split(',').join(' ');
+//         console.log(sortBy);
+//         query = query.sort(sortBy);  //sort('price ratingAverage)
+//       }else{
+//         query = query.sort('-createdAt');
+//       }
+//       return this;
+//     }
+//     limitFields(){
+//         if(this.queryString.fields){
+//             log(req.query.sort)
+//             const fields = this.queryString.fields.split(',').join(' ');
+//             // console.log(fields);
+//             this.query = this.query.sort(fields);  //sort('price ratingAverage)
+//           }
+//           else{
+//             this.query = this.query.sort('-v');
+//           }
+//           return this;
+//     }
+//     paginate(){
+//         const page = this.queryString.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+//     const skip = (page - 1) * limit;
+//     this.query = this.query.skip(skip).limit(limit);
+//     //  if(req.query.page){
+//     //    const numTours = await Tour.countDocuments();
+//     //    if(skip >= numTours) throw new Error('This page doesnot exist');       
+//     //  }
+//        return this;
+//     }
+
+// }
+
+// module.exports = APIFeatures;
+
+class APIFeatures {
+    constructor(query, queryString) {
+      this.query = query;
+      this.queryString = queryString;
+    }
+  
+    filter() {
+      const queryObj = { ...this.queryString };
+      const excludedFields = ['page', 'sort', 'limit', 'fields'];
+      excludedFields.forEach(el => delete queryObj[el]);
+  
+      // 1B) Advanced filtering
+      let queryStr = JSON.stringify(queryObj);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  
+      this.query = this.query.find(JSON.parse(queryStr));
+  
+      return this;
+    }
+  
+    sort() {
+      if (this.queryString.sort) {
+        const sortBy = this.queryString.sort.split(',').join(' ');
+        this.query = this.query.sort(sortBy);
+      } else {
+        this.query = this.query.sort('-createdAt');
+      }
+  
+      return this;
+    }
+  
+    limitFields() {
+      if (this.queryString.fields) {
+        const fields = this.queryString.fields.split(',').join(' ');
+        this.query = this.query.select(fields);
+      } else {
+        this.query = this.query.select('-__v');
+      }
+  
+      return this;
+    }
+  
+    paginate() {
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 100;
+      const skip = (page - 1) * limit;
+  
+      this.query = this.query.skip(skip).limit(limit);
+  
+      return this;
+    }
+  }
+  module.exports = APIFeatures;
+  
