99956b19bad064b624e1a619647ff45857d0fc77
hamzaali <hamza54834@gmail.com>
2021-01-20 01:51:51 +0500

    class 04

---
+++ class Authentication/class-04/models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+
+// name , email , photo , passwordConfirm
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tells us your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provider  valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'please provide a password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate:{
+            //This only works create and save
+            validator: function(el) {
+                return el === this.password
+            },
+            message: 'Password are not same'
+        }
+    }
+
+})
+
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+  
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 10);
+  
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+  });
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
